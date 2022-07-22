0b4d259b56a58805bdd5c085590376a35915cdd4
Patrick Dolan <pdolan1110@gmail.com>
2022-07-22 08:57:32 -0400

    Update anchovy.0.0.py

--- anchovy.0.0.py
+++ anchovy.0.0.py
@@ -30,32 +30,36 @@ def Initialize(args):
     Note: replaces position if alternative minimum sites are found. Producing a 3' bias? Need to consider improvements here.
     '''
     #Input SAM
-    print("\n\n----------------==============-----------------")
-    print("--==--==--==--==   ><>    ==--==--==--==--=")
-    print("--==--==--==--== ><>  ><> ==--==--==--==--=")
-    print("==--==--==--==--  anchovy --==--==--==--==-")
-    print("----------------==============-----------------\n\n")
+    print("\n\n----------------=============-----------------")
+    print("==--==--==--==--    ><>      --==--==--==--==-")
+    print("--==--==--==--==  ><>   ><>  ==--==--==--==--=")
+    print("==--==--==--==--   anchovy   --==--==--==--==-")
+    print("----------------=============-----------------\n\n")
 
 
     #Check for Args
-    if len(args)<2:
-        print("USAGE: python anchovy.0.0.py pathto/input.sam query")
+    if len(args)<3:
+        print("USAGE: python anchovy.0.0.py pathto/input.sam pathto/whitelist.txt query")
         exit()
 
     #Query
-    query=args[2]
-    print("Query Length: {}".format(len(query)))
+    query=args[3]
+    whitelist=args[2]
+
+    quL=len(query)
+    print("Query Length: {}".format(quL))
 
     #input SAM
     inputData=sys.argv[1]
     print("\nLoading SAM: {}".format(inputData))
-    pdSam=loadSAM(inputData,minL,maxL)
+    pdSam=loadSAM(inputData,quL)
+    pdCBCs=loadBC(whitelist)
     outfile=inputData.replace(".sam",'_anchovy.csv')
     print("\nOutfile: {}".format(outfile))
 
-    return(str(query),str(templateSeq), pdSam, int(Dtemp), int(cutoff), outfile)
+    return(str(query), pdCBCs, pdSam, outfile)
 
-def loadSAM(inputData, minL, maxL):
+def loadSAM(inputData,quL):
     '''
     Function: loadSAM()
         Reads in SAM file.
@@ -72,15 +76,33 @@ def loadSAM(inputData, minL, maxL):
 
     #types=[str,int,str,int,int,str,str,str,int,str,int]#SAM columns
     #typeDict=dict(zip(names,types))
-
+    minL=quL
     size=len(samInput)
     print("Total Candidate Reads: {}".format(size))
     pdSam=pd.DataFrame(samInput,columns=names)
     #print(pdSam)
     #pdSam.astype(typeDict)
     pdSam['length']=pdSam.seq.apply(lambda s: len(s))
-    pdSam=pdSam[(pdSam.length>minL)&(pdSam.length<maxL)&(pdSam.template!="*")]
+    pdSam=pdSam[(pdSam.length>minL)&(pdSam.template!="*")]
     return(pdSam)
+
+def loadBC(whitelist):
+    '''
+    Function: loadBC()
+        Reads in 10X whitelist.
+
+    Arguments:
+        "whitelist" is a string of the path to the input 20x whitelist of cell barcodes.
+    '''
+
+    with open(whitelist,"r") as IF:
+        CBCs=[lin.split("\t")[0] for lin in IF]
+    size=len(CBCs)
+    print("Total Cell Barcodes: {}".format(size))
+
+    pdCBC=pd.DataFrame(CBCs,columns=["CBC"])
+
+    return(pdCBC)
 
 def blockDist(seq_query):
     '''
@@ -98,7 +120,7 @@ def blockDist(seq_query):
     #print(seq)
     #Prepare Query
     quL=len(query)
-    minD=quL #set minimum distance to length of query
+    minD=quL #set maximum distance to length of query
 
     # Build vectorized
     BlockDistance = lambda Block: Levenshtein.distance(Block,query)
@@ -111,27 +133,57 @@ def blockDist(seq_query):
     minD=min(bDist.astype(int))
     minPos=np.argmin(bDist.astype(int))
 
-    matchlist=minPos
     matchseq=blocks[minPos]
-    contextSeq="|".join([seq[(minPos-quL):minPos],seq[min(len(seq),minPos+quL):min(len(seq),(minPos+quL))]])
-
-    return(minD,minPos,matchseq,contextSeq,matchlist)
+    return(minD, minPos, matchseq)
 
 def poolBlocks(query,pdSam,nthreads=16):#uses multithreading to compute the matches
     with Pool(nthreads) as p:
         print("\n1. Computing minimum distance hit position for {} reads.".format(len(pdSam)))
         quL=len(query)
         print("Query: {}".format(query))
-        pdSam['minD'],   pdSam['minPos'],   pdSam['matchseq'],    pdSam['context'],   pdSam['matchlist']   = zip(*p.map(blockDist, [(c.upper(),query.upper()) for c in pdSam.seq]))
-
+        pdSam['minD'],   pdSam['minPos'],   pdSam['matchseq']   = zip(*p.map(blockDist, [(c.upper(),query.upper()) for c in pdSam.seq]))
     return(pdSam)
+
+def cellMatch(matchseq, CBCs, blocks):
+    print("Query: "+matchseq)
+    #Define Levenshtein distance function
+    BlockDistance = lambda Block: Levenshtein.distance(Block,matchseq)
+
+    #Vectorize the BlockDistance function
+    vectorBlockDist = np.vectorize(BlockDistance,otypes=[int])
+
+    #Generate Block Distance vector, 'bDist'
+    bDist = vectorBlockDist(blocks)
+
+    #compute minimum and position of minimum
+    minD=min(bDist.astype(int))
+    minPos=np.argmin(bDist.astype(int))
+    matchseq=blocks[minPos]
+
+    print(CBCs.iloc[minPos])
+    print(matchseq)
+    print("\n")
+
+    return(CBCs.iloc[minPos], minD, minPos, matchseq)
+
+def cellID(pdSam, pdCBCs, nthreads=16):
+    #make reconstructed query containing CBC and constant portions of index and TSO
+    blocks=np.array(["CTACACGACGCTCTTCCGATCT"+i+"NNNNNNNNNNTTTCTTATAT" for i in pdCBCs.CBC])
+    pdSam=pdSam[pdSam.minD<30]
+    CBCtable=pd.DataFrame([cellMatch(i, pdCBCs, blocks) for i in pdSam.matchseq])
+    print(CBCtable)
+    return(CBCtable)
 
 if __name__=="__main__":
     t0=(time.time())
-    query, pdSam, outfileName = Initialize(sys.argv)
+    query, pdCBCs, pdSam, outfileName = Initialize(sys.argv)
     pdSam = poolBlocks(query,pdSam)
-    print("\nMapped hits in {} reads.".format(np.sum([int(i)<=int(cutoff) for i in pdSam.minD])))
-    pdSam[(pdSam.minPos>0)&(pdSam.minD<cutoff)].to_csv(outfileName)
+    print("\nMapped hits in {} reads.".format(np.sum([int(i)>0 for i in pdSam.minPos])))
+
+    pdSam = cellID(pdSam, pdCBCs)
+
+    pdSam[(pdSam.minPos>0)].to_csv(outfileName)
+
     t1=(time.time())
     print("Done in {} minutes.".format((t1-t0)/60))
     print("Wrote {}.".format(outfileName))
